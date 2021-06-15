0121ce4ecce831a3c9b41f61a1500807a5b8a7e7
Cerbu Gabriel <cerbugabriel@gmail.com>
2021-06-15 19:13:58 +0300

    1st copy

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,12 @@
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyAsHZ8bHbq2CXCT9CZHGiwbkef3VqMRGGw",
+  authDomain: "test-project1234-dd057.firebaseapp.com",
+  databaseURL: "https://test-project1234-dd057-default-rtdb.firebaseio.com",
+  projectId: "test-project1234-dd057",
+  storageBucket: "test-project1234-dd057.appspot.com",
+  messagingSenderId: "617681415798",
+  appId: "1:617681415798:web:46c902026c3616d8fff957",
+  measurementId: "G-W38ND3KW2J"
 };
 
 export default config;
